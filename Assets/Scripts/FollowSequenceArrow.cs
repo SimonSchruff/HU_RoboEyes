71dac9b113cecd614bdd73601e1b5b339420fa66
Simon - MacBook <simon.schruff@gmail.com>
2022-01-17 12:08:00 +0100

    New Direction Calculation

--- Assets/Scripts/FollowSequenceArrow.cs
+++ Assets/Scripts/FollowSequenceArrow.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro; 
+using System; 
 
 public class FollowSequenceArrow : MonoBehaviour
 {
+    [Header("Variables")]
     public float TimeToMoveToSquare; 
     public float TimeToRecover; 
+
+
+    [Header("References")]
     public Sprite ArrowSprite; 
     public Sprite ArrowInactiveSprite; 
     public TextMeshProUGUI DebugText; 
@@ -22,7 +27,7 @@ public class FollowSequenceArrow : MonoBehaviour
         m_sp.sprite = ArrowInactiveSprite; 
 
         DebugText.text = null; 
-        
+        CalculateDirVector(); 
     }
 
     void Update()
@@ -47,11 +52,12 @@ public class FollowSequenceArrow : MonoBehaviour
         DebugText.text = ("Robo Arm is moving towards square " + square); 
 
         //Get direction towards arrow
-        GameManager.instance.SquarePositions.TryGetValue(square, out Vector3 squarePos) ; 
-        Vector2 direction = squarePos - transform.position;  
+        //GameManager.instance.SquarePositions.TryGetValue(square, out Vector3 squarePos) ; 
+        //Vector2 direction = squarePos - transform.position;  
 
         //Rotate Arrow in that direction
-        float angle = Vector2.SignedAngle(Vector2.right, direction); 
+        //float angle = Vector2.SignedAngle(Vector2.right, direction); 
+        float angle = transform.rotation.eulerAngles.z - CalculateDirVector(); 
         transform.eulerAngles = new Vector3(0,0,angle); 
         
         yield return new WaitForSeconds(TimeToMoveToSquare);
@@ -64,5 +70,25 @@ public class FollowSequenceArrow : MonoBehaviour
         yield return new WaitForSeconds(TimeToRecover); 
 
         m_nextSequence = true; 
+    }
+
+    private float CalculateDirVector()
+    {
+        float y = 60; 
+        float x = 30 ; 
+        float z = 20; 
+
+        float c = Mathf.Sqrt((x*x) + (z * z)); 
+        print("c = " + c); 
+
+        float angle = Mathf.Asin( x/c ) * Mathf.Rad2Deg ; 
+        print(angle); 
+
+        return angle; 
+
+
+
+
+        
     }
 }
